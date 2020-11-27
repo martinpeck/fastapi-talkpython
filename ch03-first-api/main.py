d83fedfdf910fede8ee6bae32dbe4053d677a787
Martin Peck <martinjohnpeck@gmail.com>
2020-11-27 17:05:42 +0000

    basic calculator endpoint

---
+++ ch03-first-api/main.py
@@ -0,0 +1,15 @@
+import fastapi
+import uvicorn
+
+api = fastapi.FastAPI()
+
+@api.get('/api/calculate')
+def calculate():
+    
+    value = 4+2
+    
+    return {
+        'value': value
+    }
+
+uvicorn.run(api, port=8000, host="127.0.0.1")

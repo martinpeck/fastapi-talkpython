318e37791c1c611aeb3a12f17870f37d3449477e
Martin Peck <martinjohnpeck@gmail.com>
2021-04-09 14:28:58 +0000

    updates to chapter 3 main

--- ch03-first-api/main.py
+++ ch03-first-api/main.py
@@ -1,36 +1,39 @@
 from typing import Optional
 import fastapi
-from fastapi.responses import JSONResponse, 
+from fastapi.responses import JSONResponse
 import uvicorn
 
 api = fastapi.FastAPI()
 
-@api.get('/')
-def index():
-    body = "<html>" \
-        "<body style='padding:10px'>" \
-        "<h1>Welcome ot the API</h1>" \
-        "<div>" \
-        "Try it: <a href='/api/calculate?x=76&y=11'>/api/calculate?x=76&y=11</a>" \
-        "</div>" \
-        "</body>" \
-        "</html>"
 
-@api.get('/api/calculate')
+@api.get("/")
+def index():
+    body = (
+        "<html>"
+        "<body style='padding:10px'>"
+        "<h1>Welcome ot the API</h1>"
+        "<div>"
+        "Try it: <a href='/api/calculate?x=76&y=11'>/api/calculate?x=76&y=11</a>"
+        "</div>"
+        "</body>"
+        "</html>"
+    )
+    return body
+
+
+@api.get("/api/calculate")
 def calculate(x: int, y: int, z: Optional[int] = None):
 
     if z is not None and z == 0:
-        return JSONResponse(content={"error": "z must not be zero"},
-                            status_code=400)
+        return JSONResponse(content={"error": "z must not be zero"}, status_code=400)
 
     value = x + y
 
     if z is not None:
         value /= z
 
-    return {
-        'value': value
-    }
+    return {"value": value}
 
 
-uvicorn.run(api, port=8000, host="127.0.0.1")
+if __name__ == "__main__":
+    uvicorn.run(api, port=8000, host="127.0.0.1")
